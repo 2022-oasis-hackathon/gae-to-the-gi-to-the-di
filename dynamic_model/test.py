a6e219156541a84d223d657e6021859085822278
choihj105 <li0_0li2@naver.com>
2022-07-18 15:57:01 +0900

    feat : Complete translate model

--- dynamic_model/test.py
+++ dynamic_model/test.py
@@ -2,17 +2,24 @@ import cv2
 import mediapipe as mp
 import numpy as np
 from tensorflow.keras.models import load_model
+import tensorflow as tf
 
-actions = ['안녕하세요','여러분','발표','시작']
-#actions = ['hello','everyone','presentation','start']
+# 에러 : tensorflow.python.framework.errors_impl.InternalError: Blas GEMM launch failed 날때
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+  tf.config.experimental.set_memory_growth(gpu, True)
+
+#actions = ['여러분','안녕하세요','발표', ,'시작']
+actions = ['everyone','hello','presentation','start']
 seq_length = 30
 
 model = load_model('dynamic_model/models/model.h5')
-print("a")
 
 # MediaPipe hands model
+
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
+
 hands = mp_hands.Hands(
     max_num_hands=2,
     min_detection_confidence=0.5,
@@ -37,13 +44,13 @@ while cap.isOpened():
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     result = hands.process(img)
     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-
+    
     if result.multi_hand_landmarks is not None:
         for res in result.multi_hand_landmarks:
             joint = np.zeros((21, 4))
             for j, lm in enumerate(res.landmark):
                 joint[j] = [lm.x, lm.y, lm.z, lm.visibility]
-
+            
             # Compute angles between joints
             v1 = joint[[0,1,2,3,0,5,6,7,0,9,10,11,0,13,14,15,0,17,18,19], :3] # Parent joint
             v2 = joint[[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], :3] # Child joint
@@ -63,23 +70,21 @@ while cap.isOpened():
             seq.append(d)
 
             mp_drawing.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)
-
+            
             if len(seq) < seq_length:
                 continue
-
+            
             input_data = np.expand_dims(np.array(seq[-seq_length:], dtype=np.float32), axis=0)
-
             y_pred = model.predict(input_data).squeeze()
 
             i_pred = int(np.argmax(y_pred))
             conf = y_pred[i_pred]
-
             if conf < 0.9:
                 continue
 
             action = actions[i_pred]
             action_seq.append(action)
-
+            
             if len(action_seq) < 3:
                 continue
 
