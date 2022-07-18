d3247f38505777bb718e106ca7b28ee869d9f57e
namjiseong <skawltjd2000@naver.com>
2022-07-18 20:35:20 +0900

    디자인 적용

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def index():
 
 @app.route('/education') # 학습 페이지 
 def education():
-    return render_template('edu.html')
+    return render_template('education.html')
 
 
 @app.route('/canvas_image', methods=('GET', 'POST')) # ajax로 0.5초마다 이미지 받아옴 // 이미지 전송, 응답 라우트
@@ -49,7 +49,9 @@ def canvas_image():
 def translation():
     return render_template('translation.html')
 
-
+@app.route('/test') # 학습 페이지 
+def test():
+    return render_template('test.html')
 
 
 if __name__ == '__main__':
